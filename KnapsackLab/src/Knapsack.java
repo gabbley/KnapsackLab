7adbaf77f192fb72f87846ae89f0d38d511c2855
gabbley <baniquedg@bishopireton.org>
2018-02-16 13:18:08 -0500

    Attempt haha

--- KnapsackLab/src/Knapsack.java
+++ KnapsackLab/src/Knapsack.java
@@ -2,12 +2,23 @@ import java.awt.List;
 
 public class Knapsack {
 
-	public int knapsackSum(int[] w, int n, int limit){
+	public static int knapsackSum(int[] w, int n, int limit){
+		int optimalSum = 0;
 		
+		if (limit == 0)
+			optimalSum = 0;
+		else
+			while (limit > 0){
+				optimalSum += w[n] + knapsackSum(w, n--, limit-1);
+			}
+		return optimalSum;
+	}
+	
+	public static int knapsackSun(int[] w, int n, int limit, List<Integer> list){
 		
 	}
 	
-	public int knapsackSun(int[] w, int n, int limit, List<Integer> list){
+	public static void main (String[] args){
 		
 	}
 	
