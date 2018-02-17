5c839f75e1bb608d8e376756567895d4fff6c78d
gabbley <baniquedg@bishopireton.org>
2018-02-16 21:48:03 -0500

    finished simplified knapsackSum method

--- KnapsackLab/src/Knapsack.java
+++ KnapsackLab/src/Knapsack.java
@@ -1,22 +1,81 @@
-import java.awt.List;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Knapsack {
+	
+	private String outputFile = "output.txt";
 
 	public static int knapsackSum(int[] w, int n, int limit){
 		int optimalSum = 0;
+		int useSum = 0;
+		int dontUseSum = 0;
 		
-		if (limit == 0)
+		if (n<0)
 			optimalSum = 0;
-		else
-			while (limit > 0){
-				optimalSum += w[n] + knapsackSum(w, n--, limit-1);
-			}
+		else if (w[n] < limit)
+				useSum += knapsackSum(w, n-1, limit-w[n]) + w[n];
+				dontUseSum += knapsackSum(w, n-1, limit);
+				if (useSum>dontUseSum)
+					optimalSum += useSum;
+				else
+					optimalSum += dontUseSum;
+		
 		return optimalSum;
 	}
 	
-	public static int knapsackSun(int[] w, int n, int limit, List<Integer> list){
+	public static int knapsackSum(int[] w, int n, int limit, ArrayList<Integer> list){
+		int optimalSum = 0;
+		int useSum = 0;
+		int dontUseSum = 0;
+		ArrayList<Integer> listUse = new ArrayList<Integer>();
+		ArrayList<Integer> listDontUse = new ArrayList<Integer>();
 		
+		if (n<0)
+			optimalSum = 0;
+		else if (w[n] < limit)
+				listUse.add(w[n]);
+				useSum += knapsackSum(w, n-1, limit-w[n], listUse) + w[n];
+				listUse.add(w[n-1]);
+				dontUseSum += knapsackSum(w, n-1, limit, listDontUse);
+				if (useSum>dontUseSum)
+					optimalSum += useSum;
+				else
+					optimalSum += dontUseSum;
+				
+		
+		
+		return optimalSum;
 	}
+	
+	public static Scanner openFile(String filename) {
+
+		File f = new File(filename);
+		Scanner input = null;
+		try {
+			input = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			return null;
+		}
+		return input;
+	}
+//
+//	public static PrintWriter canBeOpened(String filename, int part) {
+//
+//		File f = new File(filename);
+//		PrintWriter output = null;
+//		try {
+//			output = new PrintWriter(f);
+//		} catch (FileNotFoundException e) {
+//			PrintWriter out = new PrintWriter(outputFile);
+//			out.println("Part" + part + ": Unable to Open File");
+//			return null;
+//		}
+//		return output;
+//	}
 	
 	public static void main (String[] args){
 		
